6d35a8ee75946b903e732356698e870079fa7848
phuducdo <amtdgroup.dpd@gmail.com>
2022-10-19 18:47:12 +0700

    upload BT + TH

---
+++ src/Class_and_Object_in_Java/baitap/StopWatch.java
@@ -0,0 +1,39 @@
+package Class_and_Object_in_Java.baitap;
+
+import java.util.Date;
+
+public class StopWatch {
+    private Date startTime;
+    private Date endTime;
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public StopWatch() {
+        this.startTime = new Date();
+    }
+
+    public void start() {
+        this.startTime = new Date();
+    }
+
+    public void stop() {
+        this.endTime = new Date();
+    }
+
+    public int getElapsedTime() {
+        return 300;
+    }
+
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        System.out.println(stopWatch.startTime);
+        System.out.println();
+
+    }
+}
