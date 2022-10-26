3f833a5217ca4a785c15ec3459fc0677abf721ab
phuducdo <amtdgroup.dpd@gmail.com>
2022-10-26 11:21:29 +0700

    upload baithap + thuchanh

--- src/_07_AbstractClass_and_Interface/thuchanh/Geometric_Comparable/Comparable.java
+++
@@ -1,5 +0,0 @@
-package _07_AbstractClass_and_Interface.thuchanh.Geometric_Comparable;
-
-public interface Comparable {
-    int compareTo();
-}
--- src/_07_AbstractClass_and_Interface/thuchanh/Geometric_Comparable/ComparableCircle.java
+++ src/_07_AbstractClass_and_Interface/thuchanh/Geometric_Comparable/ComparableCircle.java
@@ -1,7 +1,23 @@
 package _07_AbstractClass_and_Interface.thuchanh.Geometric_Comparable;
 
-import _06_Inheritance_in_Java.baitap.Circle_Cylinder.Circle;
+import _06_Inheritance_in_Java.thuchanh.Circle;
 
-public class ComparableCircle extends Circle {
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle> {
+    public ComparableCircle() {
+    }
 
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(String color, boolean filled, double radius) {
+        super(color, filled, radius);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
 }
