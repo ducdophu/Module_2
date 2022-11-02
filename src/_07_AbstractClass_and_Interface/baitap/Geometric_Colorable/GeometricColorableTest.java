6f0db5bd7da80b31d44a28dd80dc34e3c5d7e394
phuducdo <amtdgroup.dpd@gmail.com>
2022-11-02 15:45:32 +0700

    upload baithap + thuchanh

--- src/_07_AbstractClass_and_Interface/baitap/Geometric_Colorable/GeometricColorableTest.java
+++ src/_07_AbstractClass_and_Interface/baitap/Geometric_Colorable/GeometricColorableTest.java
@@ -1,9 +1,11 @@
 package _07_AbstractClass_and_Interface.baitap.Geometric_Colorable;
+
 public class GeometricColorableTest {
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[2];
-        shapes[0] = new Square();
-        shapes[1] = new Square(3.3);
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Shape();
+        shapes[1] = new Square();
+        shapes[2] = new Square(3.3);
 
         for (int i = 0; i < shapes.length; i++) {
             System.out.println(shapes[i]);
